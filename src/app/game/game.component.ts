737b783e7e36333bf219cd50b7f8996f793c73cd
Sebastian Wolff <sebastian.wolff@iu-study.org>
2024-01-16 21:43:26 +0100

    added logic to picking a card , added game class for players and cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -9,11 +10,29 @@ import { Component } from '@angular/core';
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
+
   pickCardAnimation = false;
+  game: Game | any;
+  currentCard: string = '';
+
   constructor() {
-    
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game;
+    console.log(this.game)
   }
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      console.log(this.currentCard)
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
+
+
   }
 }
