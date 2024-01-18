a14c66e287ca99136791e77d97d89d0ae82ebb0a
Sebastian Wolff <sebastian.wolff@iu-study.org>
2024-01-18 13:23:21 +0100

    added card for instructions when picked a card , added function for adding players, first playable version

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -4,12 +4,15 @@ import { Game } from '../../models/game';
 import { PlayerComponent } from '../player/player.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialog,MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { GameInfoComponent } from '../game-info/game-info.component';
 
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule],
+  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule, MatDialogModule, GameInfoComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
@@ -19,7 +22,7 @@ export class GameComponent {
   game: Game | any;
   currentCard: string = '';
 
-  constructor() {
+  constructor(public dialog: MatDialog) {
     this.newGame();
   }
 
@@ -34,12 +37,28 @@ export class GameComponent {
       this.pickCardAnimation = true;
       console.log("New Card:" + this.currentCard)
       console.log('Game is', this.game)
+      this.changePlayer();
       setTimeout(() => {
         this.game.playedCards.push(this.currentCard)
         this.pickCardAnimation = false;
       }, 1000);
     }
-
-
   }
+
+  changePlayer() {
+    this.game.currentPlayer++;
+    this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent)
+
+    dialogRef.afterClosed().subscribe(name => {
+      if (name && name.length > 0) {
+        this.game.players.push(name) 
+      }
+    });
+  }
+
+
 }
